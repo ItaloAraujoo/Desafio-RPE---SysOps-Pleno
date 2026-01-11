af8f5a2d18c320dc419c75f3651bdbae569be21a
italoaraujoo <italo3040@gmail.com>
2026-01-11 15:48:30 -0300

    alteração de imagem para ubuntu 22 no lugar da amazon linux

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -30,6 +30,7 @@ resource "aws_iam_role_policy_attachment" "ssm_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+
 # Política para CloudWatch Logs (opcional - para logs de aplicação)
 resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
   role       = aws_iam_role.ec2_role.name
@@ -73,7 +74,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
 # ----------------------------------------------------------------------------
 # EC2 INSTANCE
-# Instância na subnet privada com Docker/Minikube
+# Instância na subnet privada com Minikube
 # ----------------------------------------------------------------------------
 
 resource "aws_instance" "wordpress" {
@@ -82,6 +83,8 @@ resource "aws_instance" "wordpress" {
   subnet_id              = var.private_subnet_id
   vpc_security_group_ids = [var.security_group_id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  key_name               = "wordpress-key"
+
 
   # Configuração do volume root
   root_block_device {
